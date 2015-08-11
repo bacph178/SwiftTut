1d886c67ce47dc9d5d733cbf66bab6717e018b71
Phùng Hoàng Bắc <phunghoangbac@Phungs-Mac-mini.local>
2015-08-11 10:16:27 +0700

    Enumerations and Structures

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,114 +2,100 @@
 
 import UIKit
 
-class Shape {
-    var numberOfSides = 0
+enum Rank: Int {
+    case Ace = 1
+    case Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten
+    case Jack, Queen, King
+    func simpleDescription() ->String {
+        switch self {
+        case .Ace:
+            return "ace"
+        case .Jack:
+            return "jack"
+        case .Queen:
+            return "queen"
+        case .King:
+            return "king"
+        default:
+            return String(self.rawValue)
+        }
+    }
+    
+    func compares(firstRank: Rank) -> Int {
+        if firstRank.rawValue > self.rawValue {
+            return -1
+        } else if firstRank.rawValue == self.rawValue {
+            return 0
+        } else {
+            return 1
+        }
+    }
+}
+
+let ace = Rank.Ace
+let two = Rank.Two
+ace.compares(two)
+two.compares(ace)
+
+if let convertedRank = Rank(rawValue: 3) {
+    let threeDescription = convertedRank.simpleDescription()
+    threeDescription
+}
+
+enum Suit {
+    case Spades, Hearts, Diamonds, Clubs
+    func simpleDescription()->String {
+        switch self {
+        case .Spades:
+            return "spades"
+        case .Hearts:
+            return "hearts"
+        case .Diamonds:
+            return "diamonds"
+        case .Clubs:
+            return "clubs"
+        }
+    }
+    
+    func color() -> String {
+        switch self {
+        case .Spades:
+            return "black"
+        case .Clubs:
+            return "black"
+        case .Hearts:
+            return "red"
+        case .Diamonds:
+            return "red"
+        }
+    }
+}
+
+let hearts = Suit.Hearts
+let heartsDescription = hearts.simpleDescription()
+
+struct Card {
+    var rank: Rank
+    var suit: Suit
     func simpleDescription() -> String {
-        return "A shape with \(numberOfSides) sides."
+        return "the \(rank.simpleDescription()) of \(suit.simpleDescription())"
     }
 }
 
-var shape = Shape()
-shape.numberOfSides = 7
-var shapeDescription = shape.simpleDescription()
-shapeDescription
+let threeOfSpades = Card(rank: .Three, suit: .Spades)
+let threeOfSpadesDescription = threeOfSpades.simpleDescription()
 
-class NamedShape {
-    var numberOfSides: Int = 0
-    var name: String
-    
-    init (name: String) {
-        self.name = name
-    }
-    
-    func simpleDescription() -> String {
-        return "A shape with \(numberOfSides) sides."
-    }
+enum ServerResponse {
+    case Result(String, String)
+    case Error(String)
 }
 
-class Square: NamedShape {
-    var sideLength: Double
-    
-    init (sideLength: Double, name: String) {
-        self.sideLength = sideLength
-        super.init(name: name)
-        numberOfSides = 4
-    }
-    
-    func area() -> Double {
-        return sideLength * sideLength
-    }
-    
-    override func simpleDescription() -> String {
-        return "A square with sides of length \(sideLength)."
-    }
+let success = ServerResponse.Result("6:00 am", "8:09 pm")
+let failure = ServerResponse.Error("Out of cheese.")
+
+switch success {
+case let .Result(sunrise, sunset):
+    let serverResponse = "Sunrise is at \(sunrise) and sunset is at \(sunset)"
+case let .Error(error):
+    let serverResponse = "Failure... \(error)"
 }
-
-let test = Square(sideLength: 5.2, name: "my test square")
-test.area()
-test.simpleDescription()
-
-class EquilateralTriangle: NamedShape {
-    var sideLength: Double = 0.0
-    init(sideLength: Double, name: String) {
-        self.sideLength = sideLength
-        super.init(name: name)
-        numberOfSides = 3
-    }
-    
-    var perimeter: Double {
-        get {
-            return 3.0 * sideLength
-        }
-        set {
-            sideLength = newValue / 3.0
-        }
-    }
-    
-    override func simpleDescription() -> String {
-        return "An equilateral triangle with sides of length \(sideLength)."
-    }
-}
-
-var triangle = EquilateralTriangle(sideLength: 3.1, name: "a triangle")
-triangle.perimeter
-triangle.perimeter = 9.9
-triangle.sideLength
-
-class TriangleAndSquare {
-    var triangle: EquilateralTriangle {
-        willSet {
-            square.sideLength = newValue.sideLength
-        }
-    }
-    
-    var square: Square {
-        willSet {
-            triangle.sideLength = newValue.sideLength
-        }
-    }
-    
-    init(size: Double, name: String) {
-        square = Square(sideLength: size, name: name)
-        triangle = EquilateralTriangle(sideLength: size, name: name)
-    }
-}
-
-var triangleAndSquare = TriangleAndSquare(size: 10, name: "another test shape")
-triangleAndSquare.square.sideLength
-triangleAndSquare.triangle.sideLength
-triangleAndSquare.square = Square(sideLength: 50, name: "larger square")
-triangleAndSquare.triangle.sideLength
-
-class Counter {
-    var count: Int = 0
-    func incrementBy(amount: Int, numberOfTimes times: Int) {
-        count += amount * times
-    }
-}
-
-var counter = Counter()
-counter.incrementBy(2, numberOfTimes: 7)
-
-let optionalSquare: Square? = Square(sideLength: 2.5, name: "optional square")
-let sideLength = optionalSquare?.sideLength
