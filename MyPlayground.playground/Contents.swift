ca3c56420644c84372bcb6a071c63ea00d86060b
Phùng Hoàng Bắc <phunghoangbac@Phungs-Mac-mini.local>
2015-08-12 15:50:20 +0700

    Ep kieu du lieu va tuple

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,33 +2,61 @@
 
 import UIKit
 
-func repeat<Item>(item: Item, times: Int)->[Item] {
-    var result = [Item]()
-    for i in 0..times {
-        result.append(item)
-    }
-    return result
+let minValue = UInt8.min
+let maxValue = UInt8.max
+let decimalInteger = 17
+let binaryInteger = 0b10001
+let octalInterger = 0o21
+let hexadecimalInterger = 0x11
+
+// Ép kiểu dữ liệu
+
+let three = 3
+let pointOneFourOneFiveNine = 0.14159
+let pi = Double(three) + pointOneFourOneFiveNine
+// let pi2 = three + pointOneFourOneFiveNine //
+let integerPi = Int(pi)
+
+let twoThousand: UInt16 = 2_000
+let one: UInt8 = 1
+let twoThousandAndOne = twoThousand + UInt16(one)
+
+// Định nghĩa tên 1 kiểu dữ liệu
+
+typealias AudioSample = UInt16
+var maxAmplitudeFound = AudioSample.min
+
+// Kiểu dữ liệu Boolean
+
+let orangesAreOrange = true
+let turnipsAreDelicious = false
+
+
+if turnipsAreDelicious {
+    let string = "Mmm, tasty turnips!"
+} else {
+    let string = "Eww, turnios are horrible."
 }
 
-repeat("e", 3)
+let i = 1
+//if i {} //error
+if i == 1 {}
 
-enum OptionalValue<T> {
-    case None
-    case Some(T)
-}
 
-var possibleInteger: OptionalValue<Int> = .None
-possibleInteger = .Some(100)
+//tuples
 
-func anyCommonElements <T, U where T: SequenceType, U: SequenceType,
-    T.Generator.Element: Equatable, T.Generator.Element == U.Generator.Element> (lhs: T, rhs: U) -> Bool {
-        for lhsItem in lhs {
-            for rhsItem in rhs {
-                if lhsItem == rhsItem {
-                    return true
-                }
-            }
-        }
-        return false
-}
-anyCommonElements([1, 2, 3], [3])
+//http404Error is of type (Int, String)
+let http404Error = (404, "Not Found")
+http404Error.0
+http404Error.1
+
+let (statusCode, statusMessage) = http404Error
+statusCode
+statusMessage
+
+let (justTheStatusCode, _) = http404Error
+justTheStatusCode
+
+let http200Status = (statusCode: 200, description: "OK")
+http200Status.statusCode
+http200Status.description
