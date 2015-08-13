ba54bce766e47d05fa91d0657aec5a137708ce62
Phùng Hoàng Bắc <phunghoangbac@Phungs-Mac-mini.local>
2015-08-13 17:48:10 +0700

    Function Advanced

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -3,70 +3,148 @@
 import UIKit
 
 /*
-    Continue: Thực hiện thông báo đã làm xong vòng lặp iteration hiện tại, tiếp 
-    đến đoạn code sau
+    giá trị trả về của funtion là tuple có thể bị nil nên dùng giá trị optional Tuple
 */
 
-let puzzleInput = "great minds think alike"
-var puzzleOutput = ""
-for character in puzzleInput {
-    switch character {
-//    case "a", "e", "i", "o", "u", " ":
-//        continue
-//    default:
-//        puzzleOutput.append(character)
-    case "g", "r", "t", "m", "n", "d", "s", "h", "k", "l":
-        puzzleOutput.append(character)
-    default:
-        continue
+func minMax(array:[Int]) -> (min: Int, max: Int)? {
+    if array.isEmpty {
+        return nil
     }
+    var currentMin = array[0]
+    var currentMax = array[0]
+    for value in array[1..<array.count] {
+        if value < currentMin {
+            currentMin = value
+        } else if value > currentMax {
+            currentMax = value
+        }
+    }
+    return (currentMin, currentMax)
 }
-puzzleOutput
+minMax([]) == nil
+minMax([5, 3, 4, 2])
 
 /*
-    break thực hiện dừng vòng lặp hoặc bỏ qua case in câu lệnh switch
+    thêm dấu # vào trước tên tham số để khi gọi hàm tên tham số được xuất hiện
 */
+
+func containsCharacter(string: String, characterToFind: Character)->Bool {
+    for character in string {
+        if character == characterToFind {
+            return true
+        }
+    }
+    return false
+}
+containsCharacter("Hoang Bac", "H")
+
+func containsCharacter1(#string: String, #characterToFind: Character)->Bool {
+    for character in string {
+        if character == characterToFind {
+            return true
+        }
+    }
+    return false
+}
+
+containsCharacter1(string: "Hoang Bac", characterToFind: "H")
 
 /*
-    Fallthrough trong switch của Swift thì sau khi match được case nó thực hiện 
-    sẽ thoát khỏi câu lệnh switch nhưng thêm Falthrough sau đoạn lệnh được thực 
-    hiện trong case được match nó sẽ tiếp tục thực hiện các case sau
+    tham số mặc định, nếu tham số không được truyền vào thì nó sẽ lấy tham số mặc định
 */
 
-let integerToDescribe = 5
-var description = "The numver \(integerToDescribe) is"
-switch integerToDescribe {
-case 2, 3, 5 , 7, 11, 13, 17, 19:
-    description += " a prime number, and also"
-    fallthrough
-default:
-    description += " an integer."
+func join(string s1:String, toString s2:String, withJoiner joiner:String = " ") -> String {
+    return s1 + joiner + s2
 }
 
-let finalSquare = 25
-var board = [Int](count: finalSquare + 1, repeatedValue: 0)
-board[03] = +08
-board[06] = +11
-board[09] = +09
-board[10] = +02
-board[14] = -10
-board[19] = -11
-board[22] = -02
-board[24] = -08
-var square = 0
-var diceRoll = 0
-gameLoop: while square != finalSquare {
-    if ++diceRoll == 7 {
-        diceRoll = 1
+join(string: "Hello", toString: "Hoang Bac")
+join(string: "Bac", toString: "Lien", withJoiner: " yeu ")
+
+/*
+
+*/
+
+func arithmeticMean(numbers: Double...) ->Double {
+    var total: Double = 0
+    for number in numbers {
+        total += number
     }
-    switch square + diceRoll {
-    case finalSquare:
-        break gameLoop
-    case let newSquare where newSquare > finalSquare:
-        continue gameLoop
-    default:
-        square += diceRoll
-        square += board[square]
-    }
+    return total / Double(numbers.count)
 }
-square
+
+arithmeticMean(1, 2, 3, 4, 5)
+arithmeticMean(3, 8.25, 18.75)
+arithmeticMean()
+
+func anotherarithmeticMean(numbers:[Double]) ->Double {
+    var total: Double = 0
+    for number in numbers {
+        total += number
+    }
+    return total / Double(numbers.count)
+}
+anotherarithmeticMean([1, 2, 3, 4, 5])
+anotherarithmeticMean([3, 8.25, 18.75])
+anotherarithmeticMean([])
+/*
+    Định nghĩa biến trong tham số truyền vào, tiết kiệm bộ nhớ
+*/
+
+func alignRight(var string:String, count:Int, pad: Character) -> String {
+    let amountToPad = count - distance(string.startIndex, string.endIndex)
+    if amountToPad < 1 {
+        return string
+    }
+    let padString = String(pad)
+    for _ in 1...amountToPad {
+        string = padString + string
+    }
+    return string
+}
+
+let originalString = "hello"
+let paddedString = alignRight(originalString, 10, "-")
+
+/*
+    tham số inout giống với tham biến trong C, giá trị của biến thay đổi sau khi
+    gọi hàm
+*/
+
+func swapTwoInts(inout a:Int, inout b:Int) {
+    let temp = a
+    a = b
+    b = temp
+}
+
+var someInt = 3
+var anotherInt = 5
+swapTwoInts(&someInt, &anotherInt)
+someInt == 5
+anotherInt == 3
+
+/*
+    Hàm trả về một hàm khác, việc trả về một hàm có thể là cách chúng ta che giấu
+    phần thân hàm, hay ý nghĩ của hàm chỉ cung cấp api hợp lý
+*/
+
+func stepForward(input: Int) -> Int {
+    return input + 1
+}
+
+func stepBackward(input: Int) -> Int {
+    return input - 1
+}
+
+func chooseStepFuntion(backwards: Bool) -> (Int) -> Int {
+    return backwards ? stepBackward : stepForward
+}
+
+var currentValue = 3
+let moveNearerToZero = chooseStepFuntion(currentValue > 0)
+while currentValue != 0 {
+    currentValue = moveNearerToZero(currentValue)
+}
+
+/*
+    Nested khai báo và hiện thực các hàm trong 1 hàm lớn
+*/
