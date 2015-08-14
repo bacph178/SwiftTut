f9388f6f5fe08c5c3eb61d68ab5cacbfc415e1da
Phùng Hoàng Bắc <phunghoangbac@Phungs-Mac-mini.local>
2015-08-14 12:50:12 +0700

    Closure, maping, capture value

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,149 +2,63 @@
 
 import UIKit
 
-/*
-    giá trị trả về của funtion là tuple có thể bị nil nên dùng giá trị optional Tuple
-*/
-
-func minMax(array:[Int]) -> (min: Int, max: Int)? {
-    if array.isEmpty {
-        return nil
-    }
-    var currentMin = array[0]
-    var currentMax = array[0]
-    for value in array[1..<array.count] {
-        if value < currentMin {
-            currentMin = value
-        } else if value > currentMax {
-            currentMax = value
-        }
-    }
-    return (currentMin, currentMax)
+let names = ["Chris", "Alex", "Ewa", "Barry", "Daniella"]
+func backwards (s1: String, s2: String) -> Bool {
+    return s1 > s2
 }
-minMax([]) == nil
-minMax([5, 3, 4, 2])
+var reversed = sorted(names, backwards)
+var anotherReversed = sorted(names)
 
 /*
-    thêm dấu # vào trước tên tham số để khi gọi hàm tên tham số được xuất hiện
+    Closure expression syntax
 */
 
-func containsCharacter(string: String, characterToFind: Character)->Bool {
-    for character in string {
-        if character == characterToFind {
-            return true
-        }
+reversed = sorted(names, {(s1: String, s2: String) -> Bool in
+    return s1 < s2
+})
+reversed
+reversed = sorted(names, {s1, s2 in return s1 < s2})
+reversed
+
+let digitNames = [0: "Zero", 1:"One", 2:"Two", 3:"Three", 4:"Four", 5:"Five",
+    6:"Six", 7:"Seven", 8:"Eight", 9:"Nine"]
+let numbers = [16, 58, 510]
+let strings = numbers.map {
+    (var number) -> String in
+    var output = ""
+    while number > 0 {
+        output = digitNames[number % 10]! + output
+        number /= 10
     }
-    return false
-}
-containsCharacter("Hoang Bac", "H")
-
-func containsCharacter1(#string: String, #characterToFind: Character)->Bool {
-    for character in string {
-        if character == characterToFind {
-            return true
-        }
-    }
-    return false
-}
-
-containsCharacter1(string: "Hoang Bac", characterToFind: "H")
-
-/*
-    tham số mặc định, nếu tham số không được truyền vào thì nó sẽ lấy tham số mặc định
-*/
-
-func join(string s1:String, toString s2:String, withJoiner joiner:String = " ") -> String {
-    return s1 + joiner + s2
-}
-
-join(string: "Hello", toString: "Hoang Bac")
-join(string: "Bac", toString: "Lien", withJoiner: " yeu ")
-
-/*
-
-*/
-
-func arithmeticMean(numbers: Double...) ->Double {
-    var total: Double = 0
-    for number in numbers {
-        total += number
-    }
-    return total / Double(numbers.count)
-}
-
-arithmeticMean(1, 2, 3, 4, 5)
-arithmeticMean(3, 8.25, 18.75)
-arithmeticMean()
-
-func anotherarithmeticMean(numbers:[Double]) ->Double {
-    var total: Double = 0
-    for number in numbers {
-        total += number
-    }
-    return total / Double(numbers.count)
-}
-anotherarithmeticMean([1, 2, 3, 4, 5])
-anotherarithmeticMean([3, 8.25, 18.75])
-anotherarithmeticMean([])
-/*
-    Định nghĩa biến trong tham số truyền vào, tiết kiệm bộ nhớ
-*/
-
-func alignRight(var string:String, count:Int, pad: Character) -> String {
-    let amountToPad = count - distance(string.startIndex, string.endIndex)
-    if amountToPad < 1 {
-        return string
-    }
-    let padString = String(pad)
-    for _ in 1...amountToPad {
-        string = padString + string
-    }
-    return string
-}
-
-let originalString = "hello"
-let paddedString = alignRight(originalString, 10, "-")
-
-/*
-    tham số inout giống với tham biến trong C, giá trị của biến thay đổi sau khi
-    gọi hàm
-*/
-
-func swapTwoInts(inout a:Int, inout b:Int) {
-    let temp = a
-    a = b
-    b = temp
-}
-
-var someInt = 3
-var anotherInt = 5
-swapTwoInts(&someInt, &anotherInt)
-someInt == 5
-anotherInt == 3
-
-/*
-    Hàm trả về một hàm khác, việc trả về một hàm có thể là cách chúng ta che giấu
-    phần thân hàm, hay ý nghĩ của hàm chỉ cung cấp api hợp lý
-*/
-
-func stepForward(input: Int) -> Int {
-    return input + 1
-}
-
-func stepBackward(input: Int) -> Int {
-    return input - 1
-}
-
-func chooseStepFuntion(backwards: Bool) -> (Int) -> Int {
-    return backwards ? stepBackward : stepForward
-}
-
-var currentValue = 3
-let moveNearerToZero = chooseStepFuntion(currentValue > 0)
-while currentValue != 0 {
-    currentValue = moveNearerToZero(currentValue)
+    return output
 }
 
 /*
-    Nested khai báo và hiện thực các hàm trong 1 hàm lớn
+    Capture Values
+    ở đây biến runningTotal đã được tham chiếu và lưu lại ở mỗi lần gọi
 */
+func makeIncrementer(forIncrement amount:Int) -> () -> Int {
+    var runningTotal = 0
+    func incrementer() -> Int {
+        runningTotal += amount
+        return runningTotal
+    }
+    return incrementer
+}
+
+let incrementByTen = makeIncrementer(forIncrement: 10)
+incrementByTen() == 10
+incrementByTen() == 20
+incrementByTen() == 30
+/*
+    ngay cả khi tạo một hàm khác thì biến runningTotal của hàm ỉnementByTen cũng
+    được lưu lại và biến runningTotal của hàm incrementBySeven được khởi tạo và
+    tham chiếu đến một nơi khác
+*/
+let incrementBySeven = makeIncrementer(forIncrement: 7)
+incrementBySeven() == 7
+incrementByTen() == 40
+incrementBySeven() == 14
+
+let alsoIncrementByTen = incrementByTen
+alsoIncrementByTen() == 50
