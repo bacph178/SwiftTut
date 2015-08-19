46057f4fb68664567a0c68f80b65490be1fad70c
Phùng Hoàng Bắc <phunghoangbac@Phungs-Mac-mini.local>
2015-08-19 13:32:19 +0700

    Bien class - static

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -3,26 +3,52 @@
 import UIKit
 
 /*
-    Biến quan sát dùng để quan sát và trả lời các sự kiện khi giá trị được thay đổi
-    dùng cho các thuộc tính lữu trữ hoặc cả thuộc tính lưu trữ và thuộc tính tính toán
-    nếu nó được kế thừa. Không thể thêm thuộc tính quan sát vào biến tính toán 1 cách
-    trực tiếp bởi ngay trong setter của nó cũng có thể thực hiện được
-    Có thể set thuộc tính quan sát bằng từ khoá willset hoặc didset
-    Nếu chúng ta không truyền tên tham số vào 2 hàm willSet hoặc didSet thì nó sẽ
-    nhận tên mặc định đối với willSet là newValue và didSet là oldValue
+    biến class static
 */
 
-class StepCounter {
-    var totalSteps: Int = 0 {
-        willSet(newTotalSteps) {
-            newTotalSteps
-        }
+struct SomeStructure {
+    static var storedTypeProperty = "Some value."
+    static var computedTypeProperty: Int {
+        return 10
+    }
+}
+
+enum SomeEnumeration {
+    static var storedTypeProperty = "Some value."
+    static var computedTypeProperty: Int {
+        return 10
+    }
+}
+
+class SomeClass {
+    static var storedTypeProperty = "Some value."
+    static var computedTypeProperty: Int {
+        return 10
+    }
+    class var overrideableComputedTypeProperty: Int {
+        return 20
+    }
+}
+
+SomeStructure.storedTypeProperty
+SomeStructure.computedTypeProperty
+SomeEnumeration.storedTypeProperty
+SomeEnumeration.computedTypeProperty
+SomeClass.storedTypeProperty
+SomeClass.computedTypeProperty
+SomeClass.overrideableComputedTypeProperty
+
+struct AudioChannel {
+    static let thresholdLevel = 10
+    static var maxInputForallChannels = 0
+    var currentLevel: Int = 0 {
         didSet {
-            if totalSteps > oldValue {
-                totalSteps - oldValue
+            if currentLevel > AudioChannel.thresholdLevel {
+                currentLevel = AudioChannel.thresholdLevel
+            }
+            if currentLevel > AudioChannel.maxInputForallChannels {
+                AudioChannel.maxInputForallChannels = currentLevel
             }
         }
     }
 }
-let stepCounter = StepCounter()
-stepCounter.totalSteps = 200
