ae8644bf6835c1e34c40e9a074206617fe557148
Phùng Hoàng Bắc <phunghoangbac@Phungs-Mac-mini.local>
2015-08-20 08:52:45 +0700

    Subcript ~ index of list, collection...

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -3,101 +3,56 @@
 import UIKit
 
 /*
-    Các phương thức thì tham số đầu tiên mặc định là local variable
-    các biến tiếp theo sẽ mặc định là external parameter
+    cấu trúc chung
+
+subscript(index: Int) -> Int {
+    get {
+        //return an appropriate subscript value
+    }
+    set {
+        //perform a suitable setting action here
+    }
+}
+    có thể viết subscript read-only bằng cách bỏ setter và bỏ từ khoá get như sau:
+
+    subscript(index: Int) -> Int {
+        //return an appropriate value subscript value
+    }
 */
 
-class Counter {
-    var count = 0
-    func increment () {
-        count++
+struct TimesTable {
+    let multiplier: Int
+    subscript(index: Int) -> Int {
+        return multiplier * index
+    }
+}
+
+let threeTimesTable = TimesTable(multiplier: 3)
+threeTimesTable[1]
+
+struct Matrix {
+    let rows: Int, columns: Int
+    var grid: [Double]
+    init (rows: Int, columns: Int) {
+        self.rows = rows
+        self.columns = columns
+        grid = Array(count: rows * columns, repeatedValue: 0.0)
     }
     
-    func incrementBy(#amount: Int, _numberOfTimes: Int) {
-        count += amount * _numberOfTimes
+    func indexIsValidForRow(row: Int, column: Int) -> Bool {
+        return row >= 0 && row < rows && column >= 0 && column < columns
     }
     
-    func reset() {
-        count = 0
-    }
-}
-
-let counter = Counter()
-counter.increment()
-counter.count == 1
-counter.incrementBy(amount: 5, _numberOfTimes: 10)
-counter.count == 6
-counter.reset()
-counter.count == 0
-
-/*
-    từ khoá self được sử dụng khi có parameter của hàm giống với thuộc tính
-    Trong struct các method muốn modify thuộc tính cần có từ khoá mutating trước
-*/
-struct Point {
-    var x = 0.0, y = 0.0
-    func isToTheRightOfX(x: Double) -> Bool {
-        return self.x > x
-    }
-    mutating func moveByX(deltaX: Double, y detalY: Double) {
-        y += detalY
-        x += deltaX
-    }
-}
-
-var somePoint = Point(x: 4.0, y: 5.0)
-if somePoint.isToTheRightOfX(1.0) {
-    //println OK
-}
-somePoint.moveByX(2, y: 3)
-
-struct LevelTracker {
-    static var highestUnlockedLevel = 1
-    static func unlockLevel(level: Int) {
-        if level > highestUnlockedLevel {
-            highestUnlockedLevel = level
+    subscript(row: Int, column: Int) -> Double {
+        get {
+            assert(indexIsValidForRow(row, column: column), "Index out of range")
+            return grid[(row * column) + column]
         }
-    }
-    
-    static func levelIsUnlocked(level: Int) -> Bool {
-        return level <= highestUnlockedLevel
-    }
-    
-    var currentLevel = 1
-    mutating func advanceToLevel(level: Int) -> Bool {
-        if LevelTracker.levelIsUnlocked(level) {
-            currentLevel = level
-            return true
-        } else {
-            return false
+        set {
+            assert(indexIsValidForRow(row, column: column), "Index out of range")
+            grid[(row * column) + column] = newValue
         }
     }
 }
 
-class Player {
-    var tracker = LevelTracker()
-    let playerName: String
-    func completedLevel(level: Int) {
-        LevelTracker.unlockLevel(level + 1)
-        tracker.advanceToLevel(level + 1)
-    }
-    init(name: String) {
-        playerName = name
-    }
-}
-
-var player = Player(name: "Lien")
-if player.tracker.advanceToLevel(6) {
-    player.tracker.currentLevel
-} else {
-    let string = "level 6 has not yet been unlocked"
-}
-
-LevelTracker.highestUnlockedLevel
-
-player.completedLevel(10)
-if player.tracker.advanceToLevel(6) {
-    player.tracker.currentLevel
-} else {
-    let string = "level 6 has not yet been unlocked"
-}
+var matrix = Matrix(rows: 2, columns: 2)
